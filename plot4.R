2ae718fb9121d4b9fbdf65f0f82144c9949bc34a
Tim Brooks <tim.brooks@staples.com>
2014-07-13 15:23:55 -0700

    Fixing bug in plot 3 and commiting plot4 and script

--- plot4.R
+++ plot4.R
@@ -8,15 +8,31 @@ read_file <- function() {
 }
 
 create_plot <- function(data_frame) {
-  png(filename="plot3.png", width=480, height=480)
+  png(filename="plot4.png", width=480, height=480)
   midpoint <- sum(y$Date == as.Date("2007-02-01"))
+  par(mfrow=c(2, 2))
+  with(data_frame, plot.ts(Global_active_power, main="", xlab="",
+                           ylab="Global Active Power", axes=F))
+  axis(2)
+  axis(1, at=c(0, 1440, length(y$Date)), labels=c("Thu", "Fri", "Sat"))
+  box()
+  with(data_frame, plot.ts(Voltage, main="", xlab="datetime", ylab="Voltage",
+                          axes=F))
+  axis(2)
+  axis(1, at=c(0, 1440, length(y$Date)), labels=c("Thu", "Fri", "Sat"))
+  box()
   plot.ts(data_frame[, c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")], plot.type="single", main="", xlab="",
           ylab="Energy sub metering", axes=F, col=c("black", "red", "blue"))
   axis(2)
   axis(1, at=c(0, 1440, length(y$Date)), labels=c("Thu", "Fri", "Sat"))
   box()
   legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-         lwd = 1, col=c("black", "red", "blue"))
+         lwd = 1, col=c("black", "red", "blue"), bty="n")
+  with(data_frame, plot.ts(Global_reactive_power, main="", xlab="datetime",
+                           ylab="Global_reactive_power", axes=F))
+  axis(2)
+  axis(1, at=c(0, 1440, length(y$Date)), labels=c("Thu", "Fri", "Sat"))
+  box()
   dev.off()
 }
 
